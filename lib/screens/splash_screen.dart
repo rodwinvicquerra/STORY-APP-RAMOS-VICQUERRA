0110bd8c93f6dfa2dad99f093b22b6c6d0f0e18c
rodwinvicquerra <rodwinvicquerra@spup.edu.ph>
2026-05-02 13:29:00 +0800

    fix white screen on splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -64,7 +64,7 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF1A1A2E),
+      backgroundColor: const Color(0xFF12122A),
       body: Stack(
         children: [
           // Background image
